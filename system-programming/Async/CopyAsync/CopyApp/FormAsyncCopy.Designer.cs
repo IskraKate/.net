9b206d0e2dc576514e98f71dae62f8ddb8dea35d
Kate Iskra <kateiskra99@gmail.com>
2020-06-28 13:52:36 +0300

    async proj added

---
+++ system-programming/Async/CopyAsync/CopyApp/FormAsyncCopy.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FormAsyncCopy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFileFrom = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxWhere = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.labelWhere = new System.Windows.Forms.Label();
+            this.buttonWhere = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFileFrom
+            // 
+            this.buttonFileFrom.Location = new System.Drawing.Point(485, 43);
+            this.buttonFileFrom.Name = "buttonFileFrom";
+            this.buttonFileFrom.Size = new System.Drawing.Size(75, 29);
+            this.buttonFileFrom.TabIndex = 0;
+            this.buttonFileFrom.Text = "File...";
+            this.buttonFileFrom.UseVisualStyleBackColor = true;
+            this.buttonFileFrom.Click += new System.EventHandler(this.buttonFileFrom_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(570, 168);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 39);
+            this.buttonCopy.TabIndex = 2;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(114, 46);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(365, 22);
+            this.textBoxFrom.TabIndex = 3;
+            // 
+            // textBoxWhere
+            // 
+            this.textBoxWhere.Location = new System.Drawing.Point(114, 107);
+            this.textBoxWhere.Name = "textBoxWhere";
+            this.textBoxWhere.Size = new System.Drawing.Size(365, 22);
+            this.textBoxWhere.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 168);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(534, 39);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Location = new System.Drawing.Point(23, 49);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(44, 17);
+            this.labelFrom.TabIndex = 6;
+            this.labelFrom.Text = "From:";
+            // 
+            // labelWhere
+            // 
+            this.labelWhere.AutoSize = true;
+            this.labelWhere.Location = new System.Drawing.Point(23, 109);
+            this.labelWhere.Name = "labelWhere";
+            this.labelWhere.Size = new System.Drawing.Size(54, 17);
+            this.labelWhere.TabIndex = 7;
+            this.labelWhere.Text = "Where:";
+            // 
+            // buttonWhere
+            // 
+            this.buttonWhere.Location = new System.Drawing.Point(485, 104);
+            this.buttonWhere.Name = "buttonWhere";
+            this.buttonWhere.Size = new System.Drawing.Size(75, 29);
+            this.buttonWhere.TabIndex = 8;
+            this.buttonWhere.Text = "File...";
+            this.buttonWhere.UseVisualStyleBackColor = true;
+            this.buttonWhere.Click += new System.EventHandler(this.buttonWhere_Click);
+            // 
+            // FormAsyncCopy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 219);
+            this.Controls.Add(this.buttonWhere);
+            this.Controls.Add(this.labelWhere);
+            this.Controls.Add(this.labelFrom);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBoxWhere);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonFileFrom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormAsyncCopy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AscyncCopy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFileFrom;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.TextBox textBoxWhere;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.Label labelWhere;
+        private System.Windows.Forms.Button buttonWhere;
+    }
+}
+
---
+++ system-programming/Async/CopyAsyncAwaitPattern/CopyApp/FormAsyncCopy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    //Написать программу, которая копирует файл блоками по 4096 байт в указанное место.Программа должна отображать прогресс копирования с помощью элемента управления «ProgressBar». Пользователь может указать пути к файлам с помощью клавиатуры или с помощью диалогового окна, которое отображается при нажатии кнопок Файл… Запуск копирования происходит при нажатии кнопки «Копировать». Копирование файла необходимо выполнить асинхронно с использованием асинхронных делегатов.
+    public partial class FormAsyncCopy : System.Windows.Forms.Form
+    {
+
+        public FormAsyncCopy()
+        {
+            InitializeComponent();
+            progressBar1.Value = 0;
+        }
+
+        private void buttonFileFrom_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd =new OpenFileDialog();
+            DialogResult result = ofd.ShowDialog();
+
+            if (!String.IsNullOrEmpty(ofd.FileName))
+            {
+                ChooseFile(textBoxFrom, ofd.FileName);
+            }
+        } 
+
+        private void buttonWhere_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text file (.txt)|*.txt|Document (.doc)|*.doc";
+            DialogResult result = sfd.ShowDialog();
+
+            if (!String.IsNullOrEmpty(sfd.FileName))
+            {
+                ChooseFile(textBoxWhere, sfd.FileName);
+            }
+        }
+
+        private void ChooseFile(TextBox txtBox, string path)
+        {
+             txtBox.Text = path;
+        }
+
+        private async void CopyAsync()
+        {
+            buttonCopy.Enabled = false;
+            await Task.Run(() => WorkAsync());
+        }
+
+        private void WorkAsync()
+        {
+            using (var input = new FileStream(textBoxFrom.Text, FileMode.Open, FileAccess.Read))
+            using (var output = new FileStream(textBoxWhere.Text, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[4096];
+                int read;
+
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    output.Write(buffer, 0, read);
+                    float pct = (1.0f * input.Position) / input.Length * 100.0f;
+                    progressBar1.Invoke((MethodInvoker)(() => progressBar1.Value = (int)pct));
+                }
+            }
+            
+            if(progressBar1.Value == 100)
+            buttonCopy.Invoke((MethodInvoker)(() => buttonCopy.Enabled = true));
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            CopyAsync();
+        }
+    }
+}
